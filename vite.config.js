88e95c756f82150139e79d0c54af7f26870bf93b
techernoelpierre <techernoelpierre@gmail.com>
2025-01-29 10:37:41 +0400

    feat: add base in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
+  base: process.env.VITE_APP_BASE_URL || undefined,
   plugins: [
     ViteMinifyPlugin({}),
   ],
